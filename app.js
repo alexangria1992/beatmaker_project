cb6909740c4e06b33bf9a3c1c564ba6e2be5a671
Alexandre Angrignon <alex040892@gmail.com>
2024-03-11 16:33:37 +1300

    adding sounds

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
 class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
+
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
-    this.playBtn = document.querySelector(".play");
     this.index = 0;
     this.bpm = 150;
   }
   activePad() {
-    // console.log(this);
     this.classList.toggle("active");
   }
   repeat() {
     let step = this.index % 8;
-    // console.log(step);
     const activeBars = document.querySelectorAll(`.b${step}`);
-    // console.log(activeBars);
-    // console.log(step);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if pads are active
+      if (bar.classList.contains("active")) {
+        // Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
+    console.log(this);
     const interval = (60 / this.bpm) * 1000;
+
     setInterval(() => {
       this.repeat();
-      //   console.log(this);
+      console.log(this);
     }, interval);
   }
 }
 
 const drumKit = new DrumKit();
-drumKit.start();
+// drumKit.start();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playBtn.addEventListener("click", function () {
+  // console.log(this);
   drumKit.start();
 });
